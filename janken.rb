ded8f85fae50b90c50911085431a9154e0a0bd2d
月野木勝洋 <tsukinokikatsuhiro@gatsunogikatsuhironoMacBook-Air.local>
2020-03-12 17:14:09 +0900

    initial commit

--- janken.rb
+++ janken.rb
@@ -6,7 +6,7 @@ class Player
       puts "0:グー, 1:チョキ, 2:パー"
       # 変数「input_hand」にプレイヤーが入力したものを取得して代入します。
         # ヒント! 「getsメソッド」を使用します。
-      input_hand = gets.to_i
+      input_hand = gets.chomp
       # 「input_hand」(取得した値)が「0, 1, 2」のいずれかだとwhileから脱出させ、それ以外だと初めから繰り返させます。
         # ヒント! 「while」と「if」を使用します。
         # 以降の型の例に沿って実装しましょう ※実装する時場合はコメントアウトに注意しましょう
@@ -14,7 +14,7 @@ class Player
           # if 条件を書く (「input_hand」(取得した値)が「0, 1, 2」のいずれかの場合だった場合)
             # 「input_hand」(取得した値)をそのまま返します。
               # ヒント！ 「return」を使用します。
-        if input_hand == 0 || input_hand == 1 || input_hand == 2
+        if input_hand == "0" || input_hand == "1" || input_hand == "2"
           return input_hand
         else #条件以外の場合
           # 「プレイヤーに 0 ~ 2を入力させる文章」を表示させます。
@@ -22,7 +22,7 @@ class Player
           puts "もう一度数字を入力してください。"
           puts "0:グー, 1:チョキ, 2:パー"
           # 変数「input_hand」にプレイヤーが入力したものを取得して代入します。
-          input_hand = gets.to_i
+          input_hand = gets.chomp
         end #if文のend
       end #while文のend
     end
